31e3984c9126984b8cb18d0ba6e373472a63bb78
Steven Kitzes <steven.kitzes@gmail.com>
2013-12-21 11:19:51 -0800

    Fixes, include FIZZBUZZ case, add documentation
    Added missing 'FIZZBUZZ!' case.
    Fixed order of checks so that the 'FIZZBUZZ!' case will be checked first (better design).
    Added documentation and cleaned up formatting.

--- main.cpp
+++ main.cpp
@@ -10,14 +10,27 @@
 using namespace std;
 
 int main() {
-	cout << endl;
+	//	alert user to program specification
+	cout << "FizzBuzz test!  For all numbers inclusive from 1 through 100,\n";
+	cout << "print 'Fizz' if the number is divisible by 3, 'Buzz' if the number\n";
+	cout << "is divisible by 5, and 'FIZZBUZZ!' if the number is divisible by\n";
+	cout << "both!  Otherwise just print out the number.\n\n";
+
+	//	for loop iterates on i from 1 through 100
 	for( unsigned i = 1; i < 101; i++ ) {
-		if( i % 3 == 0 ) cout << "Fizz...";
-		if( i % 5 == 0 ) cout << "Buzz...";
-		if( i%3 != 0 && i%5 != 0 ) cout << i;
-		cout << endl;
+		//	if i divisible by 3 and 5, FIZZBUZZ!
+		if( i % 3 == 0 && i % 5 == 0 ) cout << "FIZZBUZZ!\n";
+		//	if i divisible by 3 and not 5, Fizz
+		else if( i % 3 == 0 ) cout << "Fizz...\n";
+		//	if i divisible by 5 and not 3, Buzz
+		else if( i % 5 == 0 ) cout << "Buzz...\n";
+		//	if not divisible by 3 and not by 5, print number
+		else cout << i << endl;
 	}
-	
+
+	//	alert user program is complete
 	cout << "Done!\n\n";
+
+	//	return
 	return 0;
-}
+}	//	main
